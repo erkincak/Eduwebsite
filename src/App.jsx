b5ab917e8b4426dbb661e487f28f29e6b4bf8982
Erkin Çak <erkincak@hotmail.com>
2024-03-10 01:48:44 +0100

    new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
+import Programs from "./Components/Programs/Programs";
+import Title from "./Components/Title/Title";
+import About from "./Components/About/About";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title subTitle="Our Programs" title=" What We Offer" />
+        <Programs />
+        <About />
+      </div>
     </div>
   );
 }
